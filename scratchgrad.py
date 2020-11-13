4b0bec8c2bb435a395576c81c1867dbf4d1acccb
Nolan Clement <nolangclement@gmail.com>
2020-11-13 10:27:48 -0800

    linear alg updates, naming conventions

--- scratchgrad.py
+++ scratchgrad.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-class tensor():
+class matrix():
     def __init__(self, d=None, dim=None, rand=False):
         if(dim != None):
             self.shape = dim
@@ -14,16 +14,22 @@ class tensor():
                 self.shape = [len(self.data), len(self.data[0])]
 
     def __str__(self):
-        return "tensor("+str(self.data)+")"
+        return "matrix("+str(self.data)+")"
 
     def __add__(self, other):
-        if(isinstance(other, tensor)):
+        if(isinstance(other, matrix)):
             return ewadd(self, other)
         else:
             return add(self, other)
 
+    def __sub__(self, other):
+        if(isinstance(other, matrix)):
+            return ewadd(self, mul(other,-1))
+        else:
+            return add(self, other*-1)
+
     def __mul__(self, other):
-        if(isinstance(other, tensor)):
+        if(isinstance(other, matrix)):
             return ewmul(self, other)
         else:
             return mul(self, other)
@@ -39,10 +45,13 @@ def itr(x):
                 yield(i,j)
 
 def mapf(x, f):
-    z = tensor(dim=x.shape)
-    for i,j in itr(x):
-        z.data[i][j] = f(x.data[i][j])
-    return z
+    if(x.shape[0] == 1):
+        return matrix([f(i) for i in x.data])
+    else:
+        z = matrix(dim=x.shape)
+        for i,j in itr(x):
+            z.data[i][j] = f(x.data[i][j])
+        return z
 
 def add(x, c):
     f = lambda x : x+c
@@ -54,42 +63,44 @@ def mul(x, c):
 
 def ewadd(x, y):
     assert x.shape == y.shape
-    z = tensor(dim=x.shape)
+    if(x.shape[0] == 1):
+        return matrix([x.data[i]+y.data[i] for i in range(x.shape[1])])
+    z = matrix(dim=x.shape)
     for i, j in itr(x):
         z.data[i][j] = x.data[i][j] + y.data[i][j]
     return z
 
 def ewmul(x, y):
     assert x.shape == y.shape
-    z = tensor(dim=x.shape)
+    z = matrix(dim=x.shape)
     for i, j in itr(x):
         z.data[i][j] = x.data[i][j] * y.data[i][j]
     return z
 
 def dot(x, y):
-    dimx = x.shape
-    dimy = y.shape
-    assert dimx[1] == dimy[0]
-    prod = tensor(dim=[dimx[0],dimy[1]])
-    if(prod.shape == [1,1]):
-        prod = 0
+    #vector dot product
+    if(1 in x.shape and 1 in y.shape):
+        a = flatten(x).data
+        b = flatten(y).data
+        return sum([i*j for (i,j) in zip(a, b)])
+    #matrix multiplication
+    else:
+        dimx = x.shape
+        dimy = y.shape
+        prod = matrix(dim=[dimx[0],dimy[1]])
+        assert dimx[1] == dimy[0]
         for i in range(dimx[0]):
             for j in range(dimy[1]):
                 for k in range(dimx[1]):
-                    prod += x.data[i] * y.data[k][j]
+                    prod.data[i][j] += x.data[i][k] * y.data[k][j]
         return prod
-    for i in range(dimx[0]):
-        for j in range(dimy[1]):
-            for k in range(dimx[1]):
-                prod.data[i][j] += x.data[i][k] * y.data[k][j]
-    return prod
 
 def transposed(x):
     dim = x.shape
     if(dim[0] == 1):
-        z = tensor([[x.data[i]] for i in range(len(x.data))])
+        z = matrix([[x.data[i]] for i in range(len(x.data))])
     else:
-        z = tensor([[x.data[j][i] for j in range(len(x.data))] for i in range(len(x.data[0]))])
+        z = matrix([[x.data[j][i] for j in range(len(x.data))] for i in range(len(x.data[0]))])
     z.shape = dim[::-1]
     return z
 
@@ -108,12 +119,10 @@ def flatten(x):
     for i in range(len(x.data)):
         for j in range(len(x.data[i])):
             flat.append(x.data[i][j])
-    return tensor(flat)
+    return matrix(flat)
 
 def tsum(x):
     sm=0
     for i,j in itr(x):
         sm += x.data[i][j]
     return sm
-
-    
